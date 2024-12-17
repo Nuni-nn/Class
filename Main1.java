3d11e936373253b485335998b14089fd8f7d135d
Nuni-nn <nurai110906@gmail.com>
2024-12-17 12:20:44 +0500

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,3 +1,23 @@
+public class OnlineBanking {
+    public String name;
+    public double balance;
+    public int Deposit;
+
+    public void setValues(String name, double balance, int Deposit) {
+        this.name = name;
+        this.balance = balance;
+        this.Deposit = Deposit;
+    }
+
+    public String getValues() {
+        String info= "Name: " + name + "\nBalance1: " + balance+"\n"+"Deposit: " + Deposit+"\n";
+        String info2= "Balance2: ";
+        if (Deposit > 0)
+            info2+= balance + Deposit + "\n";
+
+        return info + info2;
+    }
+}
 
 public class Main{
     public static void main(String[] args){
